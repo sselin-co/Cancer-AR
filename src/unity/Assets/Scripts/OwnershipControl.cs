21bae45eee5987734fb574f8e14332a6464ebc65
Parsa <ParsaRajabiPR@gmail.com>
2021-03-17 16:37:45 -0700

    Testing 2 way model ownership

---
+++ src/unity/Assets/Scripts/OwnershipControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OwnershipControl : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+
+    public void OnMouseDown()
+    {
+        photonView.RequestOwnership();
+    }
+    
+    void Start()
+    {
+        // Debug.Log("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Once per frame");
+    }
+}
