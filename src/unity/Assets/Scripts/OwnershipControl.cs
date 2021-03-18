0843df2b3f8fbc24d82b502a3219a71e14184b81
Parsa <ParsaRajabiPR@gmail.com>
2021-03-18 13:06:45 -0700

    added debug.log to ownershipcontrol script

--- src/unity/Assets/Scripts/OwnershipControl.cs
+++ src/unity/Assets/Scripts/OwnershipControl.cs
@@ -9,10 +9,12 @@ public class OwnershipControl : MonoBehaviourPun
     void Start()
     {
         Lean.Touch.LeanTouch.OnFingerTap += HandleFingerTap;
+        Debug.Log("Tap Detected in Ownership Script");
     }
 
     void HandleFingerTap(Lean.Touch.LeanFinger finger)
     {
+        Debug.Log("Changing Ownership");
         photonView.RequestOwnership();
     }
 }
