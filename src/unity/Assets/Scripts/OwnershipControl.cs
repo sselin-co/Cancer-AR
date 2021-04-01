19deeb8f907cd3d461a8ee8c90ced7437c2f2e2e
Parsa <ParsaRajabiPR@gmail.com>
2021-03-31 23:04:44 -0700

    Updated OwnershipControl.cs

--- src/unity/Assets/Scripts/OwnershipControl.cs
+++ src/unity/Assets/Scripts/OwnershipControl.cs
@@ -1,20 +1,32 @@
 ﻿using UnityEngine;
 using Photon.Pun;
+using UnityEngine.EventSystems;
 
 public class OwnershipControl : MonoBehaviourPun
 {
     private void OnMouseDown()
     {
         Debug.Log("Changing Ownership from Mouse Input");
-        base.photonView.RequestOwnership();
+        TransferOwnership();
     }
 
     private void Update()
     {
-        if (Input.touchCount > 0)
+        // check for touch input that is NOT over a UI element i.e a button
+        if (Input.touchCount > 0 && !EventSystem.current.IsPointerOverGameObject())
         {
             Debug.Log("Touch Detected to Change Ownership");
-            base.photonView.RequestOwnership();
+            TransferOwnership();
+        }
+    }
+
+    private void TransferOwnership()
+    {
+        // do not allow ownership transfer if annotate is active
+        if (!Annotate.isAnnotateActive)
+        {
+            // transfer ownership (ensure the prefab's ownership is set to "TakeOver" - read photon doc for more info
+            photonView.RequestOwnership();
         }
     }
 }
